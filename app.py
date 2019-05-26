d56adac22807de0d30e1eca4e548762ebc7be655
Kristian Stafford Smith <kristianss7890@gmail.com>
2019-05-26 19:51:56 +0100

    able to GET trial index of orders from db

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask
-from pony.orm import Database
+from pony.orm import Database, db_session
 
 app = Flask(__name__)
 db = Database()
 db.bind(provider='postgres', dbname='final-project-db')
 
 # pylint: disable=W0611,C0413
-from models.Order import Order # loads in the models and controllers
+from models.Order import Order, OrderSchema # loads in the models and controllers
 
 db.generate_mapping(create_tables=True)
 
+order_schema = OrderSchema(many=True)
+
 @app.route('/')
-def index():
+def home():
     return 'Hello there', 200
+
+@app.route('/orders')
+@db_session
+def index():
+    orders = Order.select()
+
+    return order_schema.dumps(orders), 200
--- models/Order.py
+++ models/Order.py
@@ -1,4 +1,5 @@
 from pony.orm import Required, Optional
+from marshmallow import Schema, fields
 from app import db
 
 class Order(db.Entity):
@@ -6,3 +7,10 @@ class Order(db.Entity):
     postcode = Required(str)
     delivery_method = Required(str)
     contents = Optional(str)
+
+class OrderSchema(Schema):
+    id = fields.Int(dump_only=True)
+    location = fields.String(required=True)
+    postcode = fields.String(required=True)
+    delivery_method = fields.String(required=True)
+    contents = fields.String()
